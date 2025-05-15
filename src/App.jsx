00f4e1dd5b60126b8b8e92843fe40e2178adfdfc
MDevHub <musbaudeenabdulrahman040@gmail.com>
2025-05-15 02:08:34 +0100

    Changed footer path

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header'
 import Page1 from './components/Page1'
 import Contact from './components/Contact'
 import Products from './components/products'
-import Footer from './components/footer'
+import Footer from './components/Footer'
 
 function App() {
   return (
